31168b9127c6fceabccdb1991437406e5fb3ed4e
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-08-01 12:21:28 +0300

    implement HomeView and HomeViewBody with custom app bar

---
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_bookly/core/utils/assets.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const CustomAppBar(),
+      ],
+    );
+  }
+}
+
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0,vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo,height: 18,),
+          const Spacer(),
+          IconButton(onPressed: (){},
+           icon: const FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24.0, 
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
