cad6531b88e0bea9ba5c7e1ffc39d000c473899b
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-08-01 10:16:50 +0300

    create splash view body

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(body: SplashViewBody());
   }
 }
