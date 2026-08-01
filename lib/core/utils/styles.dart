6c093f8bb6db3d6327c8465ea1ccbbd9e91ed0ea
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-08-01 15:47:50 +0300

    refactor: update text styles in Styles class and modify HomeViewBody to use new textStyle18 for 'Best Seller' title

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const TextStyle titleMedium = TextStyle(
+  static const TextStyle textStyle20 = TextStyle(
     fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w600,
   );
 }
--- lib/features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_bookly/core/utils/assets.dart';
 import 'package:my_bookly/core/utils/styles.dart';
 import 'package:my_bookly/features/home/data/presentation/views/widgets/best_saler_list_view_item.dart';
 import 'package:my_bookly/features/home/data/presentation/views/widgets/custom_app_bar.dart';
@@ -20,7 +19,7 @@ class HomeViewBody extends StatelessWidget {
       
           const FeatuedBooksListView(),
           const SizedBox(height: 50),
-          Text('Best Seller', style:Styles.titleMedium),
+          Text('Best Seller', style:Styles.textStyle18,),
           const BestsalerListViewItem(),
         ],
       ),
