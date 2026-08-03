68294ca2791d11d9428bc2261ff8d9a987944317
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-08-03 15:02:14 +0300

    add BookRating widget and integrate it into BestsalerListViewItem

--- lib/features/home/data/presentation/views/widgets/best_saler_list_view_item.dart
+++ lib/features/home/data/presentation/views/widgets/best_saler_list_view_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_bookly/constant.dart';
 import 'package:my_bookly/core/utils/assets.dart';
 import 'package:my_bookly/core/utils/styles.dart';
+import 'package:my_bookly/features/home/data/presentation/views/widgets/book_rating.dart';
 
 class BestsalerListViewItem extends StatelessWidget {
   const BestsalerListViewItem({super.key});
@@ -24,36 +25,40 @@ class BestsalerListViewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 30),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(fontFamily: kFontFamily),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              const SizedBox(height: 3),
-             const Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(height: 3),
-              Row(
-                children:  [
-                  Text(
-                    '19.99€',
-                    style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
+                    style: Styles.textStyle20.copyWith(fontFamily: kFontFamily),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                 
-              ])
-            ],
+                ),
+                const SizedBox(height: 3),
+               const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(height: 3),
+                Row(
+                  children:  [
+                    Text(
+                      '19.99€',
+                      style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                 const Spacer(),
+                    const BookRating(),
+                ])
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
+
