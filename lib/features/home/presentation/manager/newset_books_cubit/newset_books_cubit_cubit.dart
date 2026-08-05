6fd9da4f9fd6fbe22e5301a6d7f2b67a320b30c6
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-08-05 18:31:13 +0300

    Implement NewsetBooksCubit with state management and fetchNewsetBooks method

--- lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit_cubit.dart
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:my_bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:my_bookly/features/home/data/repos/home_repo.dart';
 
 part 'newset_books_cubit_state.dart';
 
 class NewsetBooksCubitCubit extends Cubit<NewsetBooksCubitState> {
-  NewsetBooksCubitCubit() : super(NewsetBooksCubitInitial());
+  NewsetBooksCubitCubit(this.homeRepo) : super(NewsetBooksCubitInitial());
+    final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksCubitLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+      (failure) => emit(NewsetBooksCubitFailure(failure.errorMessage)),
+      (books) => emit(NewsetBooksCubitSuccess(books)),
+    );
+  }
 }
